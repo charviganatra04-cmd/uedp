a55e0236b73189b4a5ac470795c05e6ae22c4832
UEDP Design System <dev@antigravity.local>
2026-09-07 20:18:51 +0530

    chore: remove boilerplate components for manual addition

--- design-system-storybook/src/index.ts
+++ design-system-storybook/src/index.ts
@@ -1,13 +1,5 @@
-// Single Barrel Export for all Figma Preserved Layer React Components
-export * from './components/Component336/Component336';
-export * from './components/Filters/Filters';
-export * from './components/Component315/Component315';
-export * from './components/DateFilters/DateFilters';
-export * from './components/Zones/Zones';
-export * from './components/Map/Map';
-export * from './components/Button/Button';
-export * from './components/Input/Input';
-export * from './components/Card/Card';
-export * from './components/Header/Header';
-export * from './components/SearchBar/SearchBar';
-export * from './components/Container/Container';
+// Single Barrel Export for React Components
+// Export components here as you add them manually, e.g.:
+// export * from './components/MyComponent/MyComponent';
+
+export {};
